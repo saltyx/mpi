9bfab07db1a00f3b357c8c086e65f12789200392
saltyx <saltyx@163.com>
2015-08-09 16:11:21 +0800

    have fixed something

--- mpi/multiply_mpi/multiply.c
+++ mpi/multiply_mpi/multiply.c
@@ -26,6 +26,7 @@
  *-----------------------------------------------------------------------------*/
 #define N 4
 #define M 2
+
 void Transpose(double *b,double *tmp ,int n, int row, int col);
 /* 
  * ===  FUNCTION  ======================================================================
@@ -35,7 +36,7 @@ void Transpose(double *b,double *tmp ,int n, int row, int col);
  */
 	void
 Read_vec (double *local_a,
-		 double *local_b,
+		double *tmp,
 		 int local_n,
 		 int n,
 		 int myrank,
@@ -43,7 +44,6 @@ Read_vec (double *local_a,
 {
 	double *a = NULL;
 	double *b = NULL;
-	double *tmp = NULL;
 	if (myrank == 0)
 	{
 		a = malloc( n*sizeof(double) );
@@ -62,17 +62,15 @@ Read_vec (double *local_a,
 			b[i] = i ;
 			printf("%lf ", b[i]);
 		}
-		tmp = malloc( n*sizeof(double));
-		Transpose(b, tmp, n, N, M);//将b转置之后放入tmp中
-		for(i=0;i<n;i++)
-			printf("\ntmp is %lf\n", tmp[i]);
-		MPI_Scatter(tmp, local_n, MPI_DOUBLE, local_b, local_n, MPI_DOUBLE, 0, comm);
+		Transpose(b, tmp, n, M, N);//将b转置之后放入tmp中
+		MPI_Bcast(tmp, M*N,MPI_DOUBLE, 0,MPI_COMM_WORLD);
+		
 		MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE, 0, comm);
 		free(a);
 		free(b);
-		free(tmp);
+		
 	}else{
-		MPI_Scatter(tmp, local_n, MPI_DOUBLE, local_b, local_n, MPI_DOUBLE, 0, comm);
+		MPI_Bcast(tmp, N*M, MPI_DOUBLE, 0,MPI_COMM_WORLD);
 		MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE, 0, comm);	
 	}	
 }		/* -----  end of function Read_vec  ----- */
@@ -95,11 +93,12 @@ Gather_vec (
 	double *t = NULL;
 	int i = 0;
 	if (myrank == 0){
-		t = malloc(n*sizeof(double));
+		t = malloc(N*N*sizeof(double));
 		MPI_Gather(local_c, local_n, MPI_DOUBLE, t, local_n, MPI_DOUBLE, 0, comm);
-		printf("result:\n");
-		for(i=0;i<n;i++)
-			printf("%lf ", t[i]);
+		printf("\nresult:\n");
+		for(i=0;i<N*N;i++)if((i+1)%4 == 0){
+			printf("%lf\n", t[i]);
+		}else printf("%lf ", t[i]);
 		putchar('\n');
 		free(t);
 	}else{
@@ -141,16 +140,36 @@ Multiply ()
 
 	int local_n = (M*N)/size;
 	double *local_a = malloc(local_n*sizeof(double));
-	double *local_b = malloc(local_n*sizeof(double));
-	double *local_c = malloc(local_n*sizeof(double));
+	double *local_b = malloc(M*N*sizeof(double));
+	double *local_c = malloc(N*sizeof(double));
 	Read_vec(local_a, local_b, local_n, (M*N), myrank, MPI_COMM_WORLD);
 	int i = 0;
+	int j = 0;
+	if(myrank == 0){
+		printf("local_b:\n");
+		for(i=0;i<M*N;i++)
+			printf("%lf ", local_b[i]);
+		putchar('\n');
+		for(i=0;i<local_n;i++)
+			printf("local_a is %lf\n", local_a[i]);
+	}
+	double temp = 0;
 	for (i=0;i<local_n;i++)
 		local_c[i] = 0.0;
-	for(i =0 ;i<local_n; i++){
-		local_c[i] += local_a[i]*local_b[i];
+	for(i =0 ;i<N; i++){
+		temp = 0;
+		for(j=0;j<local_n;j++)
+			temp += local_a[j]*local_b[i*M+j];
+		local_c[i] = temp;
 	}
+	if (myrank == 0)
+		for(i=0;i<N;i++)
+			printf("local_c : %lf\n", local_c[i]);
+//	MPI_Barrier(MPI_COMM_WORLD);
 	Gather_vec(local_c, local_n, M*N, myrank, MPI_COMM_WORLD);	
+	free(local_a);
+	free(local_b);
+	free(local_c);
 	MPI_Finalize();
 }		/* -----  end of function Multiply  ----- */
 /* 
